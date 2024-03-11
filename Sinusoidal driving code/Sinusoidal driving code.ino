0a9be60c6487de3e173cc8fb14cec0b0e1d3f47f
Lezgin Alimoğlu <lezgin2003@gmail.com>
2024-03-11 21:04:33 +0300

    Update Sinusoidal driving code.ino

--- Sinusoidal driving code/Sinusoidal driving code.ino
+++ Sinusoidal driving code/Sinusoidal driving code.ino
@@ -19,17 +19,28 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
 #define USMIN  1000 // This is the rounded 'minimum' microsecond 
 #define USMAX  2000 // This is the rounded 'maximum' microsecond 
 #define SERVO_FREQ 240 // Frequency of servo
+#define pi 3.1415926535897932384626433832795
 
+
+
+
+int dc_OffsetVec[12] = {1240,1360,1500,1500,1500,1500,1500,1500,1500,1500,1500,1500}; //This array is representing our initial position, adjust the value according to your motor position.
+int i = 0;
+int amplitude = 500;
 
 
 
 void motor(int number, float phase){                          
-  int k = (-SERVOMIN + SERVOMAX)/2; 
-  float a = phase * 3.14 / 24;                                   // Change this section according to your uses, I prefer to divide phase to 24
-  length = (SERVOMIN + SERVOMAX)/2 + k*sin((i*3.14/400 + a));    //Some transformation again for pulse length count
+  float a = (phase * pi) / 24;                                   // Change this section according to your uses, I prefer to divide phase to 24
+  int dc_Offset = dc_OffsetVec[number];
+  float length = dc_Offset + amplitude*sin(((i*pi)/100 + a));    //Some transformation again for pulse length count
   pwm.setPWM(number, 0, length);                                 //This function comes from library
 }
 
+void motor_set(int number){
+  pwm.setPWM(number, 0, dc_OffsetVec[number]);
+
+}
 
 
 
@@ -46,39 +57,20 @@ void setup() {
 
 void loop() {
 
-  for (uint16_t i = 0; i < 400; i++) {
-    motor(0,0);
-    motor(1,1);
-    motor(2,2);
-    motor(3,3);
-    motor(4,4);
-    motor(5,5);
-    motor(6,6);                                    // Left number repsresent which motor will work.
-    motor(8,8);                                    // Right number indicates phase haw many n*pi/400.
-    motor(10,10);
-    motor(11,11);
-    motor(13,13);
-    motor(14,14);
-    }
-    
-
-  /*for (uint16_t i = 400; i > 0; i--) {
-    motor(0,0);
-    motor(1,1);
-    motor(2,2);
-    motor(3,3);
-    motor(4,4);
-    motor(5,5);                                   // This part is for reverse direction.
-    motor(6,6);
-    motor(8,8);
-    motor(10,10);
-    motor(11,11);
-    motor(13,13);
-    motor(14,14);
-    }
-  */
-
-      
+  motor(0,0);
+  motor(1,1);
+  motor(2,2);
+  motor(3,3);
+  motor(4,4);
+  motor(5,5);
+  motor(6,6);
+  motor(7,7);
+  motor(8,8);
+  motor(9,9);
+  motor(10,10); 
+  motor(11,11);
   
-  //pwm.setPWM(0, 0, n(3.14*0/12));              // This code comes from the library.
+  i++;
+    
+    
 }
