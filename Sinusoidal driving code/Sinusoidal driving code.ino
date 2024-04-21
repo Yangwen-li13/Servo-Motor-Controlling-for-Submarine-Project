03fb311eee4cd29acba6cf0f4961b2ddc7bf8a1b
Lezgin Alimoğlu <lezgin2003@gmail.com>
2024-04-21 21:32:42 +0300

    Update Sinusoidal driving code.ino

--- Sinusoidal driving code/Sinusoidal driving code.ino
+++ Sinusoidal driving code/Sinusoidal driving code.ino
@@ -23,7 +23,6 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
 
 
 
-
 int dc_OffsetVec[12] = {1512,1535,1532,1495,1490,1530,1520,1522,1527,1525,1555,1547}; //This array is representing our initial position, adjust the value according to your motor position.
 int i = 0;
 int amplitude = 0;
@@ -31,16 +30,19 @@ int amplitude_values = 500;
 
 
 
+
 void motor(int number, float phase){                          
   float a = (phase * pi) / 24;                                   // Change this section according to your uses, I prefer to divide phase to 24
   int dc_Offset = dc_OffsetVec[number];
-  float length = dc_Offset + amplitude*sin(((i*pi)/100 + a));    //Some transformation again for pulse length count
+  float length = dc_Offset + amplitude*sin((millis()*pi/1000 + a));    //Some transformation again for pulse length count
   pwm.setPWM(number, 0, length);                                 //This function comes from library
 }
 
+
+
+
 void motor_set(int number){
   pwm.setPWM(number, 0, dc_OffsetVec[number]);
-
 }
 
 
@@ -54,8 +56,8 @@ void setup() {
   pwm.setPWMFreq(SERVO_FREQ);                                
   delay(10);
 
-  while (amplitude< amplitude_values) {
-    amplitude += 20;
+  while (amplitude< amplitude_values ) {
+
     motor(0,0);
     motor(1,1);
     motor(2,2);
@@ -68,7 +70,11 @@ void setup() {
     motor(9,9);
     motor(10,10); 
     motor(11,11);
-    i++;
+    if(millis() % 200 < 0.5) {
+      amplitude += 100;
+    }
+    delay(10);
+    
   }
 }
 
@@ -81,21 +87,26 @@ void loop() {
     Serial.println(rc);
 
     if (rc == 's') {
-      while (amplitude > 0) {
-        motor(0, 0);
-        motor(1, 1);
-        motor(2, 2);
-        motor(3, 3);
-        motor(4, 4);
-        motor(5, 5);
-        motor(6, 6);
-        motor(7, 7);
-        motor(8, 8);
-        motor(9, 9);
-        motor(10, 10);
-        motor(11, 11);
-        amplitude -= 20;
-      }
+    while (0 < amplitude ) {
+
+      motor(0,0);
+      motor(1,1);
+      motor(2,2);
+      motor(3,3);
+      motor(4,4);
+      motor(5,5);
+      motor(6,6);
+      motor(7,7);
+      motor(8,8);
+      motor(9,9);
+      motor(10,10); 
+      motor(11,11);
+    if(millis() % 200 < 0.5) {
+      amplitude -= 100;
+    }
+    delay(10);
+    
+  }
     }
 
     if (rc >= '0' && rc <= '9') {
@@ -121,12 +132,6 @@ void loop() {
   i++;
 
 
-  
-
-
-  
-
-    
 
     
 }
