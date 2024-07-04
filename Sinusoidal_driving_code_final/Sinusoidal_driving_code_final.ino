cf7006777e155a95158bf209e70f1253d9a44ec9
Lezgin Alimoğlu <lezgin2003@gmail.com>
2024-07-04 15:21:46 +0300

    Update Sinusoidal_driving_code_final.ino

--- Sinusoidal_driving_code_final/Sinusoidal_driving_code_final.ino
+++ Sinusoidal_driving_code_final/Sinusoidal_driving_code_final.ino
@@ -26,23 +26,21 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
 
 int dc_OffsetVec[NUM_MOTORS] = {1532,1580,1527,1482,1494,1450,1530,1530,1500,1530,1535,1547}; //This array is representing our initial position, adjust the value according to your motor position.
 
-int amplitude0 = 0;
-float phase0 = 0;
-float frequency0 = 0;
+int amplitude = 0;
+float phase = 0;
+float frequency = 0;
+int amplitude_final = 400;
 
 
 int amplitude_value = 400;                        //This is approximately maximum value
 float frequency_value = 0.5;                      //This is fixed frequency value, the maximum can be 0.6 and the minimum can be 0.1 
 float phase_value;
-int test_amplitude;
-int test_phase;
-int test_frequency;
 
  
 void motor(int number, float phase){                          
   phase_value = (phase * pi) / 24;                                                                             // Change this section according to your uses, I prefer to divide phase to 24
   int dc_Offset = dc_OffsetVec[number];
-  float length = dc_Offset + amplitude0*sin((millis() *(2 * pi * frequency_value) / 1000 + phase_value));    //Some transformation again for pulse length count
+  float length = dc_Offset + amplitude*sin((millis() *(2 * pi * frequency_value) / 1000 + phase_value));    //Some transformation again for pulse length count
   pwm.setPWM(number, 0, length);                                                                               //This function comes from library
 }     //This function is essential to give PWM to motors
 
@@ -59,33 +57,33 @@ void motor_set(int number){
 }                                                                           //This function is to turn motors initial positions
 
 void stop_motor() {
-    while (0 < amplitude0) {
-    motor(0,0);
-    motor(1,1);
-    motor(2,2);
-    motor(3,3);
-    motor(4,4);
-    motor(5,5);
-    motor(6,6);
-    motor(7,7);
-    motor(8,8);
-    motor(9,9);
-    motor(10,10); 
-    motor(11,11);
-    if(millis() % 200 < 1.2) {
-      amplitude0 -= 50;
-    if (0 > amplitude0){
-        amplitude0 = 0;
-        
+    while (0 < amplitude) {
+      motor(0,0);
+      motor(1,1);
+      motor(2,2);
+      motor(3,3);
+      motor(4,4);
+      motor(5,5);
+      motor(6,6);
+      motor(7,7);
+      motor(8,8);
+      motor(9,9);
+      motor(10,10); 
+      motor(11,11);
+      if(millis() % 200 < 5) {
+        amplitude -= 25;
+      if (0 > amplitude){
+          amplitude = 0;
+
+      }
     }
-  }
   delay(10);
   }
 }                                                                            //This function is to stop motors
 
 
 void start_motor(int target_amplitude) {
-  while (amplitude0 < target_amplitude) {
+  while (amplitude < target_amplitude) {
     motor(0,0);
     motor(1,1);
     motor(2,2);
@@ -98,10 +96,10 @@ void start_motor(int target_amplitude) {
     motor(9,9);
     motor(10,10); 
     motor(11,11);
-    if(millis() % 200 < 1.5) {
-      amplitude0 += 25;
-    if(amplitude0 > target_amplitude){
-      amplitude0 = target_amplitude;
+    if(millis() % 200 < 3.5) {
+      amplitude += 25;
+    if(amplitude > target_amplitude){
+      amplitude = target_amplitude;
     }
   }
   delay(10);
@@ -109,22 +107,22 @@ void start_motor(int target_amplitude) {
 }                                                                            //This function is to start motors.             
 
 
-void changeParameters(int max_amplitude0, int percentage, float new_frequency){          //Don't forget to stop motors before using this function!!!!
-  float constantA_copy = max_amplitude0 * (percentage / 100.0);
-  int constantA = max_amplitude0 * (percentage / 100.0);
-  int amplitude0_a = 0;
+void changeParameters(int max_amplitude, int percentage, float new_frequency){          //Don't forget to stop motors before using this function!!!!
+  float constantA_copy = max_amplitude * (percentage / 100.0);
+  int constantA = max_amplitude * (percentage / 100.0);
+  int amplitude_a = 0;
   int durationStop = 5000;
   int durationWorking = 10000;
 
 
-  while(amplitude0_a < max_amplitude0){
+  while(amplitude_a < max_amplitude){
     
     if(constantA != constantA_copy){
       Serial.println("Percentage cannot divide amplitude as integer!");
       break;
     }
     
-    if(0 > max_amplitude0 or 400 < max_amplitude0){
+    if(0 > max_amplitude or 400 < max_amplitude){
       Serial.println("Amplitude is wrong! Check maximum(400) and minimum(0) values!");
       break;
     }
@@ -136,20 +134,19 @@ void changeParameters(int max_amplitude0, int percentage, float new_frequency){ 
     stop_motor();
     delay(durationStop);
     frequency_value = new_frequency;
-    amplitude0_a = amplitude0_a + constantA;
-    //amplitude0 = amplitude0_a;
+    amplitude_a += constantA;
+
     Serial.println("Amplitude is :");
-    Serial.print(amplitude0_a);
-    start_motor(amplitude0_a);
+    Serial.println(amplitude_a);
+    start_motor(amplitude_a);
     
-    float Time1 = millis();
-    float Time2 = millis();
-    while ( (Time2 - Time1) < durationWorking) {
+    unsigned long startTime = millis();
+    while ( (millis() - startTime) < durationWorking) {
       motorCont();
-      Time2 = millis();
     }
   }
   Serial.println("Test has been done!");
+  amplitude_final = amplitude_a;
 }                                                                           
 
 
@@ -170,7 +167,6 @@ void setup() {
 
 
 void loop() {
-  
   char rc;
   if (Serial.available()) {
     rc = Serial.read();
@@ -181,18 +177,19 @@ void loop() {
     }                           //Stops motor
     
     if(rc == 'c'){
-      start_motor(amplitude0_a);
+      start_motor(amplitude_final);
     }
     
     if (rc == 'a') {
       Serial.println("Changing is Started");
-      stop_motor();
-      amplitude0 = 0; 
-      frequency0 = 0;
+      int amplitude2 = 0; 
+      float frequency2 = 0;
       int percentage = 0;
+      stop_motor();
+
       Serial.println("Enter the amplitude value");
-      while(amplitude0 == 0){
-        amplitude0 = Serial.parseInt();
+      while(amplitude2 == 0){
+        amplitude2 = Serial.parseInt();
       }
       delay(10);
       Serial.println("Enter the percentage of amplitude value");
@@ -201,16 +198,16 @@ void loop() {
       }
       delay(10);
       Serial.println("Enter the frequency value");
-      while(frequency0 == 0){
-        frequency0 = Serial.parseFloat();
+      while(frequency2 == 0){
+        frequency2 = Serial.parseFloat();
       }
       delay(10);
 
-      changeParameters(amplitude0, percentage, frequency0);
+      changeParameters(amplitude2, percentage, frequency2);
       
   
     }
   }
-  motorCont();
   
+  motorCont();  
 }
