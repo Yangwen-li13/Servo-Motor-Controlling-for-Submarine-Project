4b5928ef7cb2df1fd6e9856014431f913b9e06ce
Lezgin Alimoğlu <lezgin2003@gmail.com>
2024-07-09 22:46:28 +0300

    Update dataSend.m

--- matlabDataSend/dataSend.m
+++ matlabDataSend/dataSend.m
@@ -1,12 +1,12 @@
 clear all
 portArd = serialportlist("available");
-arduino = serialport(portArd, 9600, "Timeout", 10);
+arduino = serialport(portArd, 9600, "Timeout", 20);
 configureTerminator(arduino, "LF");
 pause(2);
 %{
-sendMessage(arduino, 'stop', 10)   : Stops motors
-sendMessage(arduino, 'start', 10)   : Starts motors
-sendMessage(arduino, 'test', 10)   : Changes variables to test motors
+sendMessage(arduino, 'stop', 20)   : Stops motors
+sendMessage(arduino, 'start',20)   : Starts motors
+sendMessage(arduino, 'test', 20)   : Changes variables to test motors
 %}
 
 dataLine = readline(arduino);
