539c369d116fdaa3b4f4d9d62c13091a4af378f7
Lezgin Alimoğlu <lezgin2003@gmail.com>
2024-07-14 17:21:08 +0300

    Update dataSend.m

--- (5) Exponential-envelope/dataSend.m
+++ (5) Exponential-envelope/dataSend.m
@@ -7,21 +7,37 @@ configureCallback(arduino, "terminator", @callbackSerial);
 configureTerminator(arduino, "LF");
 
 % Initialize variables
+parametersVectors = [];
 timeVectors = [];
 periodNumber = 0;
 timeVectorIndex = 1;
 
 % Callback function for serial communication
 function callbackSerial(src, ~)
-    persistent timeVectors periodNumber timeVectorIndex;
+    persistent timeVectors periodNumber timeVectorIndex parametersVectors;
     
     if isempty(timeVectors)
         timeVectors = []; % Initialize if empty
     end
+    if isempty(parametersVectors)
+        parametersVectors = [];
+    end
+
     
     data = readline(src);
     disp(data);
     
+
+    if startsWith(data, "Amplitude:")
+        values = sscanf(data, 'Amplitude: %d, Period: %d, Frequency: %f');
+        amp = values(1);
+        per = values(2);
+        freq = values(3);
+        save('parameters.mat', 'amp', 'per', 'freq');
+        disp('Parameters saved to parameters.mat');
+
+    end
+
     % Check for test period message
     if startsWith(data, "Test Period is:")
         periodNumber = str2double(readline(src));
@@ -33,20 +49,18 @@ function callbackSerial(src, ~)
         disp(['Time Vector Value: ', num2str(value)]);
         
         % Ensure the timeVectors matrix can accommodate the new period and index
-        %if size(timeVectors, 1) < periodNumber
+
             timeVectors(periodNumber, timeVectorIndex) = value;
-        %end
+
         timeVectorIndex = timeVectorIndex + 1;
     elseif strcmp(strtrim(data), "Test has been done! Write new command")
         disp("Test has been done! Write new command.");
-        saveTimeVectors(timeVectors); % Save the time vectors to a .mat file
-        load("timeVectors.mat");
-        %wave(timeVectors,4,400,25,0.5);
+        save('timeVectors.mat', 'timeVectors');
+        disp('Time vectors saved to timeVectors.mat');
+        plotReferenceWave();
     end
 end
 
-% Function to save time vectors to a .mat file
-function saveTimeVectors(timeVectors)
-    save('timeVectors.mat', 'timeVectors');
-    disp('Time vectors saved to timeVectors.mat');
-end
+
+
+
