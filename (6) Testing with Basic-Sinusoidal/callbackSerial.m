3a11bfab8f75d240682124084748db4bf2bc0245
Lezgin Alimoğlu <lezgin2003@gmail.com>
2024-07-14 22:42:49 +0300

    Update callbackSerial.m

--- (6) Testing with Basic-Sinusoidal/callbackSerial.m
+++ (6) Testing with Basic-Sinusoidal/callbackSerial.m
@@ -1,21 +1,30 @@
 function callbackSerial(src, ~)
     persistent timeVectors periodNumber timeVectorIndex parametersVectors;
     
+    % Initialize variables if they are empty
+    
     if isempty(timeVectors)
-        timeVectors = []; % Initialize if empty
+        timeVectors = []; 
     end
     if isempty(parametersVectors)
         parametersVectors = [];
     end
 
-    
+    % Read data if it is not empty
     data = readline(src);
     if isempty(data)
         return;
     end
     disp(data);
     
+    % When test begin, Create a text file
+    if strcmp(strtrim(data), "Test began")
+    text = fopen('command.txt','w');
+    fprintf(text, '%c', 'Start');
+    fclose(text);
+    end
 
+    % Save parameters
     if startsWith(data, "Amplitude:")
         values = sscanf(data, 'Amplitude: %d, Period: %d, Frequency: %f');
         amp = values(1);
@@ -23,10 +32,9 @@ function callbackSerial(src, ~)
         freq = values(3);
         save('parameters.mat', 'amp', 'per', 'freq');
         disp('Parameters saved to parameters.mat');
-
     end
 
-    % Check for test period message
+    % Check for test period message, save vectors and change text file
     if startsWith(data, "Test Period is:")
         periodNumber = str2double(readline(src));
         disp(['Period Number: ', num2str(periodNumber)]);
@@ -35,13 +43,12 @@ function callbackSerial(src, ~)
         % Extract the time vector value
         value = str2double(readline(src));
         disp(['Time Vector Value: ', num2str(value)]);
-        
-        % Ensure the timeVectors matrix can accommodate the new period and index
-
-            timeVectors(periodNumber, timeVectorIndex) = value;
-
+        timeVectors(periodNumber, timeVectorIndex) = value;
         timeVectorIndex = timeVectorIndex + 1;
     elseif strcmp(strtrim(data), "Test has been done! Write new command")
+        text = fopen('command.txt','w');
+        fprintf(text, '%c', 'Stop');
+        fclose(text);
         save('timeVectors.mat', 'timeVectors');
         disp('Time vectors saved to timeVectors.mat');
         plotReferenceWave();
